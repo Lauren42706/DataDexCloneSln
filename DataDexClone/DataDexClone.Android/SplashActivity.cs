4a19c92584e50a4b305ae4d46e90dbf9889560a3
Lauren42706 <4270683@myuwc.ac.za>
2022-05-10 16:06:19 +0200

    Splash screen has deployment errors

---
+++ DataDexClone/DataDexClone.Android/SplashActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDexClone.Droid
+{
+    [Activity(Label = "SplashActivity", MainLauncher = true, Theme = "@style/NewTheme.Splash", NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+
+        protected override async void OnResume()
+        {
+            base.OnResume();
+            await SimulateStartup();
+        }
+
+        async Task SimulateStartup()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(8));
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
--- DataDexClone/DataDexClone.Android/obj/Debug/120/android/environment.armeabi-v7a.s
+++ DataDexClone/DataDexClone.Android/obj/Debug/120/android/environment.armeabi-v7a.s
@@ -105,7 +105,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"ef06e30b-638d-4e55-884f-8ec02af13d97"
+	.asciz	"733be79b-4763-47be-ae8f-a556921f8ae7"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
